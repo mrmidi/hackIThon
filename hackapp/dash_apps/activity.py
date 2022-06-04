bacd5bc29364be6e4dc118d8bd9117aeaf2ed476
mrmidi <mistermidi@gmail.com>
2022-06-04 02:31:46 +0200

    Added steps by day

--- hackapp/dash_apps/activity.py
+++ hackapp/dash_apps/activity.py
@@ -13,21 +13,51 @@ from dash.dependencies import Input, Output
 
 app = DjangoDash('activity', external_stylesheets=[dbc.themes.BOOTSTRAP])
 
+def get_steps_by_day(df):
+    users_count = df.user.nunique()
+    df = df.resample('D', on='datetime').sum()
+    df.reset_index(inplace=True)
+    #df = df.resample('D', on='datetime')
+    df['steps'] = df['steps'].div(users_count)
+    df['datetime'] = df['datetime'].dt.day_name()
+    #df.index = pd.to_datetime(df.index)
+    #df['d'] = df['datetime']
+    print(df.info())
+    print(df)
+    print(type(df))
+    fig = px.bar(df, x='datetime', y='steps', title='Steps by Day')
+    return fig
 
 
-dfmy = pd.read_csv('static/data/activity-minutes-1.csv')  # open csv file
-dfmy.loc[:, 'date'] = pd.to_datetime(dfmy['date'].astype(str) + ' ' + dfmy['time'].astype(str))  # join date & time
-dfmy = dfmy.drop('time', 1)  # delete time column
-print(dfmy.head(3))
-print(dfmy.info())
-mycols = [{"name": i, "id": i} for i in dfmy.columns]
+df = pd.read_csv('static/data/data_home.csv', parse_dates=['datetime'])
+dfs = pd.read_csv('static/data/data-_school.csv', parse_dates=['datetime'])
+users = df.user.unique()
 
-print(f"columns: {mycols}")
-tbl = dt.DataTable(
-    data=dfmy.to_dict('records'),
-    columns=mycols
-)
-print(type(dfmy['date']))
+# users = df_h.user.unique()
+# users_count = df_h.user.nunique()
+# print(users)
+# print(type(users))
+# print(df_h)
+# print(df_h.info())
+# df_h = df_h.resample('D', on='datetime').sum()
+# df_h['steps'] = df_h['steps'].div(users_count)
+# print(df_h)
+
+#print(df.head(5))
+
+# dfmy = pd.read_csv('static/data/activity-minutes-1.csv')  # open csv file
+# dfmy.loc[:, 'date'] = pd.to_datetime(dfmy['date'].astype(str) + ' ' + dfmy['time'].astype(str))  # join date & time
+# dfmy = dfmy.drop('time', 1)  # delete time column
+# print(dfmy.head(3))
+# print(dfmy.info())
+# mycols = [{"name": i, "id": i} for i in dfmy.columns]
+
+# print(f"columns: {mycols}")
+# tbl = dt.DataTable(
+#     data=dfmy.to_dict('records'),
+#     columns=mycols
+# )
+# print(type(dfmy['date']))
 #s = date.fromtimestamp(dfmy['date'].iloc[0])
 #e = date.fromtimestamp(dfmy['date'].iloc[-1])
 #print(f"start: {s} end {e}")
@@ -35,16 +65,16 @@ print(type(dfmy['date']))
 # start_date =
 # end_date =
 
-date_filter = dbc.FormGroup([
-    dbc.Label('Period', html_for="date-filter"),
-    dcc.DatePickerRange(
-        id='date-filter',
-        min_date_allowed=date(2019, 10, 7),
-        max_date_allowed=date(2021, 3, 1),
-        initial_visible_month=date(2019, 10, 7),
-        end_date=date(2019, 10, 14)
-    ),
-])
+# date_filter = dbc.FormGroup([
+#     dbc.Label('Period', html_for="date-filter"),
+#     dcc.DatePickerRange(
+#         id='date-filter',
+#         min_date_allowed=date(2019, 10, 7),
+#         max_date_allowed=date(2021, 3, 1),
+#         initial_visible_month=date(2019, 10, 7),
+#         end_date=date(2019, 10, 14)
+#     ),
+# ])
 
 
 
@@ -53,33 +83,54 @@ date_filter = dbc.FormGroup([
 # from dash.dependencies import Input, Output
 
 app.layout = html.Div([
-    date_filter,
-    html.Div(id='output-container-date-picker-range'),
-    html.Div(
-        tbl
+    html.Div(id='dd-output-container'),
+    dcc.Dropdown(
+        id="users-dropdown",
+        placeholder='All Users',
+        value=None,
+        options=[{'label': f'User #{user}', 'value': user} for user in users]),
+    dcc.Graph(
+         id='dd-output-graph'
     )
 ])
+
+#
+#
+# @app.callback(
+#     Output('output-container-date-picker-range', 'children'),
+#     Input('date-filter', 'start_date'),
+#     Input('date-filter', 'end_date'))
+# def update_output(start_date, end_date):
+#     string_prefix = 'You have selected: '
+#     if start_date is not None:
+#         start_date_object = date.fromisoformat(start_date)
+#         start_date_string = start_date_object.strftime('%B %d, %Y')
+#         string_prefix = string_prefix + 'Start Date: ' + start_date_string + ' | '
+#     if end_date is not None:
+#         end_date_object = date.fromisoformat(end_date)
+#         end_date_string = end_date_object.strftime('%B %d, %Y')
+#         string_prefix = string_prefix + 'End Date: ' + end_date_string
+#     if len(string_prefix) == len('You have selected: '):
+#         return 'Select a date to see it displayed here'
+#     else:
+#         return string_prefix
 
 
 
 @app.callback(
-    Output('output-container-date-picker-range', 'children'),
-    Input('date-filter', 'start_date'),
-    Input('date-filter', 'end_date'))
-def update_output(start_date, end_date):
-    string_prefix = 'You have selected: '
-    if start_date is not None:
-        start_date_object = date.fromisoformat(start_date)
-        start_date_string = start_date_object.strftime('%B %d, %Y')
-        string_prefix = string_prefix + 'Start Date: ' + start_date_string + ' | '
-    if end_date is not None:
-        end_date_object = date.fromisoformat(end_date)
-        end_date_string = end_date_object.strftime('%B %d, %Y')
-        string_prefix = string_prefix + 'End Date: ' + end_date_string
-    if len(string_prefix) == len('You have selected: '):
-        return 'Select a date to see it displayed here'
-    else:
-        return string_prefix
+    #Output('dd-output-container', 'children'),
+    Output('dd-output-graph', 'figure'),
+    Input('users-dropdown', 'value')
+)
+def update_steps_by_day(value):
+    return get_steps_by_day(filter_data(value, df))
 
 
 
+
+def filter_data(user, df):
+    filtered_df = df.copy()
+    if user is not None:
+        filtered_df = filtered_df[filtered_df['user'] == user]
+
+    return filtered_df
--- runposgres.sql
+++ runposgres.sql
@@ -1,2 +1,1 @@
-docker run -d --name pg-hackithon -e POSTGRES_PASSWORD=challenge2022 -e PGDATA=/var/lib/posgresql/data/pgdata -e POSTGRES_DB=hackithon -v /Users/mrmidi/POSTGRES:/var/lib/postgresql/data 
--p 5432:5432 postgres
+docker run -d --name pg-hackithon -e POSTGRES_PASSWORD=challenge2022 -e PGDATA=/var/lib/posgresql/data/pgdata -e POSTGRES_DB=hackithon -v /Users/mrmidi/POSTGRES:/var/lib/postgresql/data -p 5432:5432 postgres
